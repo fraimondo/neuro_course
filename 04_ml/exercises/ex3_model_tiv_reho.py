856faac5a8a946fc6eaacfc1ddfea4b7dacf2919
Fede <fraimondo@proton.me>
2024-04-19 12:52:46 -0300

    leakage in CV average

--- 04_ml/exercises/ex3_model_tiv_reho.py
+++ 04_ml/exercises/ex3_model_tiv_reho.py
@@ -4,9 +4,30 @@ from julearn.api import run_cross_validation
 from julearn.pipeline import PipelineCreator
 import pandas as pd
 import seaborn as sns
+from sklearn.svm import LinearSVC
 
 # %%
 storage = HDF5FeatureStorage("./data/ds003097_ReHo.hdf5")
+
+a = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13" "14",
+    "15",
+    "16",
+    "17",
+    "18",
+]
 
 # %%
 df = storage.read_df("BOLD_ReHo-Power2013-10mm")
@@ -29,11 +50,16 @@ df_full = df.merge(df_demographics, on="subject")
 df_full = df_full.merge(df_volumne, on="subject")
 
 
-
 # %%
 creator = PipelineCreator(problem_type="classification")
 creator.add("zscore")
-creator.add("svm", C=[0.1, 1, 10, 100])
+creator.add(
+    LinearSVC(),
+    name="linearsvm",
+    C=[0.001, 0.01, 0.1, 1, 10, 100],
+    penalty="l1",
+    dual=False,
+)
 # %%
 scores, model, inspector = run_cross_validation(
     X=X,
@@ -46,19 +72,13 @@ scores, model, inspector = run_cross_validation(
 
 # %%
 predictions = inspector.folds.predict()
-to_merge = df_full[["sex", "eTIV"]].iloc[
-    predictions.index
-]
+to_merge = df_full[["sex", "eTIV"]].iloc[predictions.index]
 to_plot = pd.concat([predictions, to_merge], axis=1)
-
-
 
 
 # %%
 to_plot["correct"] = to_plot["repeat0_p0"] == to_plot["target"]
 # %%
-sns.boxplot(
-    data=to_plot, x="sex", hue="correct", y="eTIV"
-)
+sns.boxplot(data=to_plot, x="sex", hue="correct", y="eTIV")
 
 # %%
